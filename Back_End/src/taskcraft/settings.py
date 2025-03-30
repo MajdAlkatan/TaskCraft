0788d5816b058c701e2b4a8521f8193cacc6e0dc
MaroofAt <maroof95at@gmail.com>
2025-03-30 11:08:55 +0300

    Created Users and Workspaces apps and added them to settings.py | Created Tools folder with tools.py (added TimeStampedModel for auto add the created_at and updated_at fields) | Installed Pillow package and added it to requirmenets.txt

--- Back_End/src/taskcraft/settings.py
+++ Back_End/src/taskcraft/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'dotenv',
 
     #own
+    'src.Users.apps.UsersConfig',
+    'src.Workspaces.apps.WorkspacesConfig',
 
 ]
 
@@ -132,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'Media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
