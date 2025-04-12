0560401d7e5bfb0b398d9904801ca67da8108ea6
MaroofAt <maroof95at@gmail.com>
2025-04-12 20:36:12 +0300

    Installed the django-silk package and Added it to requirements.txt and added the route to silk to the project urls.py

--- Back_End/src/taskcraft/settings.py
+++ Back_End/src/taskcraft/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'corsheaders',
     'django_filters',
+    'silk',
 
     #own
     'users',
@@ -71,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'taskcraft.urls'
