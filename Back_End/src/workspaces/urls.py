b33b8f52ff47e0c0e00bfc838fddfaed7eff4954
MaroofAt <maroof95at@gmail.com>
2025-04-20 17:33:16 +0300

    makemigration for workspace_invitaions table

--- Back_End/src/workspaces/urls.py
+++ Back_End/src/workspaces/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import WorkspaceViewSet , CreateWorkspaceInvitationLink , GetWorkspaceInvitationLink
+from .views import WorkspaceViewSet , CreateWorkspaceInvitationLink , GetWorkspaceInvitationLink , JoinWorkspaceViaInvitationLink
 
 router = DefaultRouter()
 router.register(r'workspaces' , WorkspaceViewSet)
@@ -10,5 +10,5 @@ urlpatterns = [
     *router.urls,
     path('create-invite-link/<int:workspace_id>/' , CreateWorkspaceInvitationLink.as_view()),
     path('get-invite-link/<int:workspace_id>/' , GetWorkspaceInvitationLink.as_view()),
-    # path('invite-link/<str:invitation_token>/' , ))
+    path('invite-link/<str:invitation_token>/' , JoinWorkspaceViaInvitationLink.as_view())
 ]
